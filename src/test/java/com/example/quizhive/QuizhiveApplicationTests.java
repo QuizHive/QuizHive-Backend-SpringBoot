ae900796536b4fd39d6197b2eba3a6430906f308
DnyaNvB <nvbdnya@gmail.com>
2025-01-16 15:21:19 +0330

    add the base project

---
+++ src/test/java/com/example/quizhive/QuizhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
